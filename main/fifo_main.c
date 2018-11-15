8e1d4c39c8b7de856105eddfbee5739ab4ffe57f
chen-zifeng <m18819838082@163.com>
2018-11-15 11:07:27 +0800

    test prepare

--- main/fifo_main.c
+++ main/fifo_main.c
@@ -12,10 +12,13 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "spiram_fifo.h"
+#include "spiram.h"
+
 
 void app_main()
 {
-    printf("Hello world!\n");
+    printf("esp32 fifo test!\n");
 
     /* Print chip information */
     esp_chip_info_t chip_info;
@@ -30,11 +33,6 @@ void app_main()
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
     fflush(stdout);
-    esp_restart();
+
 }
